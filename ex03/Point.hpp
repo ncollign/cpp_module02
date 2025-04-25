72e2dbb44f69398e2e7e9734c6948137c54589bb
ncollign <nicolas.collignon7@gmail.com>
2025-04-25 10:51:36 +0200

    ex03 en cours

---
+++ ex03/Point.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncollign <ncollign@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 10:43:24 by ncollign          #+#    #+#             */
+/*   Updated: 2025/04/25 10:48:49 by ncollign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_HPP
+# define POINT_HPP
+# include "Fixed.hpp"
+
+class Point
+{
+	private:
+		const Fixed _x;
+		const Fixed _y;
+
+	public:
+		Point();
+		Point(float const x, float const y);
+		Point(Point const &other);
+		Point &operator=(Point const &other);
+		~Point();
+
+		Fixed getX() const;
+		Fixed getY() const;
+};
+
+#endif
